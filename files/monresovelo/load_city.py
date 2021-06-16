b7176a1cd2dbcbdd2e51bb05546c76f2d6765ef2
Lucas Magnana <lucas.magnana@gmail.com>
2021-06-16 17:35:03 +0200

    first commit

---
+++ files/monresovelo/load_city.py
@@ -0,0 +1,11 @@
+import osmnx as ox
+import os
+import pickle
+
+G = ox.graph_from_point((45.516955, -73.567614), 1500)
+
+if not os.path.exists(os.path.dirname("files/monresovelo/city_graphs/city.ox")):
+    os.makedirs(os.path.dirname("files/monresovelo/city_graphs/city.ox"))
+
+with open("files/monresovelo/city_graphs/city.ox", "wb") as outfile:
+    pickle.dump(G, outfile)
